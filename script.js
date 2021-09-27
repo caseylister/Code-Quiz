2c4186c72afd7db6a02801af2265e46f939459ae
caseylister <caseynlister@gmail.com>
2021-09-27 15:47:11 -0400

    created initial files

---
+++ script.js
@@ -0,0 +1,21 @@
+var quiz = document.getElementById("quiz");
+var prequiz = document.getElementById("prequiz");
+var choiceA = document.getElementById("a");
+var choiceB = document.getElementById("b");
+var choiceC = document.getElementById("c");
+var choiceD = document.getElementById("d");
+var finalScoreEl = document.getElementById("finalScore");
+var endQuiz = document.getElementById("endQuiz");
+var questionsEl = document.getElementById("questions");
+var timer = document.getElementById("timer");
+var startBtn = document.getElementById("startbutton");
+var highscoreContainer = document.getElementById("container");
+var highscorePage = document.getElementById("score-page");
+var scoreName = document.getElementById("name");
+var highscoreName = document.getElementById("score-name");
+var finishQuizEl = document.getElementById("finishQuiz");
+var submit = document.getElementById("submit-score");
+var showScoreEl = document.getElementById("score-score");
+
+
+
